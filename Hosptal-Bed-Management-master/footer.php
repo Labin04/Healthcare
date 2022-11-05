b65dbd45b4640051969bf8fb1ef444eb367c3b79
Labin04 <90048789+Labin04@users.noreply.github.com>
2022-11-05 09:17:04 +0530

    Add files via upload

---
+++ Hosptal-Bed-Management-master/footer.php
@@ -0,0 +1,12 @@
+
+                <div class="clear"></div>
+            </div>
+            <!-- // #container -->
+        </div>	
+        <!-- // #containerHolder -->
+        
+        <p id="footer"><h1><span><img src="2.png" ></span></h1></p>
+    </div>
+    <!-- // #wrapper -->
+</body>
+</html>
